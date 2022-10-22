af806bc0299f62bac421f334e8590def36719431
Aziz Soorti <hies.aziz2019@gmail.com>
2022-10-22 11:17:29 -0700

    form data storing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import Cards from './components/Cards';
 import contacts from './contacts';
 import './App.css';
+import Login from './components/Login';
 
 function App() {
+  // const creatF = (contacts) => {
+  //   return (
+  //   <Cards
+  //   key= {contacts.id}
+  //   name={contacts.name}
+  //   img={contacts.img_url}
+  //   phone={contacts.phone}
+  //   email={contacts.email}
+  // />
+  //   )
+  // }
+
   return (
     <>
-      <Cards
+
+<Login />
+
+      {/* {contacts.map((contacts) => {
+        return (
+          <Cards
+            key={contacts.id}
+            name={contacts.name}
+            img={contacts.img_url}
+            phone={contacts.phone}
+            email={contacts.email}
+          />
+        )
+
+      })} */}
+
+
+      {/* <Cards
         name={contacts[0].name}
         img={contacts[0].img_url}
         phone={contacts[0].phone}
@@ -24,7 +54,16 @@ function App() {
       img={contacts[2].img_url}
       phone={contacts[2].phone}
       email={contacts[2].email}
-      />
+      /> */}
+
+      {/* <Cards
+        name="Name: Faiz"
+        img_url="https://www.thenews.com.pk/assets/uploads/akhbar/2022-08-09/980792_5962407_faiz_akhbar.jpg"
+        phone="Phone: 0123456789"
+        email="Email: abc@gmail"
+      /> */}
+
+
     </>
   );
 }
---
+++ src/components/Login.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+
+function Login() {
+
+    const [name, setName] = useState();
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+
+  
+    const [allEntry, setAllEntry] = useState();
+    const submitForm = (e) => {
+        e.preventDefault();
+        var newEntry = {
+            name: name,
+            email: email,
+            password: password
+        }
+        console.log(newEntry);
+    }
+
+    return (
+        <div style={{
+            width: "350px", height: "400px", backgroundColor: "white", borderRadius: "20px", margin: "100px auto",
+            boxShadow:
+                "rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px"
+
+        }}>
+
+            <h1 style={{ marginBottom: "20px", textAlign: "center" }} >Login Form</h1>
+            <form onSubmit={(e) => { submitForm(e) }}>
+                <input onChange={(e) => { setName(e.target.value) }} style={{ display: "block", margin: "0 1rem", width: "100%", borderRadius: "16px", borderColor: "white" }} type="text" placeholder='Username' />
+                <input onChange={(e) => { setEmail(e.target.value) }} style={{ display: "block", margin: "30px 0", width: "100%", borderRadius: "16px", borderColor: "white" }} type="text" placeholder='Email' />
+                <input onChange={(e) => { setPassword(e.target.value) }} style={{ display: "block", width: "100%", borderRadius: "16px", borderColor: "white" }} type="text" placeholder='Password' />
+                <button type='Submit' style={{ background: "skyblue", borderRadius: "16px", width: "70%", borderColor: "skyblue", color: "white", margin: "70px 15%" }}>Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default Login
+
